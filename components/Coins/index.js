91fa78cf4b84ed69a8b642849b1a77a2554ff004
qtpham <quincytpham@gmail.com>
2021-05-03 22:25:39 -0700

    Have all initial data displayed with formatting

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,9 +1,35 @@
-const Coins = ({name}) => {
+import styles from './Coins.module.css';
+const Coins = ({ name, id, price, symbol, marketcap, volume, image, priceChange }) => {
     return (
-        <div>
-            <h1>{name}</h1>
+        <div className={styles.coin_container}>
+            <div className={styles.coin_row}>
+                <div className={styles.coin}>
+                    <img src={image} alt={image} className={styles.coin_img}/>
+                    <h1 className={styles.coin_h1}>{name}</h1>
+                    <p className={styles.coin_symbol}>{symbol}</p>
+                </div>
+                <div className={styles.coin_data}>
+                    <p className={styles.coin_price}>${price}</p>
+                    <p className={styles.coin_volume}>${volume.toLocaleString()}</p>
+
+                    {priceChange < 0 ? (
+                        <p className={styles.coin_percent, styles.red}>
+                            {priceChange.toFixed(2)}%
+                        </p>
+                    ) : (
+                        <p className={styles.coin_percent, styles.green}>
+                            {priceChange.toFixed(2)}%
+                        </p>
+                    )}
+
+                    <p className={styles.coin_marketcap}>
+                        Mkt Cap: ${marketcap.toLocaleString()}
+                    </p>
+                </div>
+            </div>
+
         </div>
-    )
-}
+    );
+};
 
 export default Coins
