a4c1e9a6164f0aec56e5bf7343d7ba5e25759412
qtpham <quincytpham@gmail.com>
2021-05-10 20:30:45 -0700

    Added homepage link, subreddit link, image link, added hover effects for links and image

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -6,12 +6,15 @@ const Coin = ( {coin} ) => {
         <Layout>
             <div className={styles.coin_page}>
                 <div className={styles.coin_container}>
-                    <img src={coin.image.large} alt={coin.name} className={styles.coin_image}/>
-            <h1 className={styles.coin_name}>{coin.name}</h1>
+                    <a href={coin.links.homepage[0]} target="_blank"><img src={coin.image.large} alt={coin.name} className={styles.coin_image}/></a>
+                    <h1 className={styles.coin_name}>{coin.name}</h1>
 
-            <p className={styles.coin_symbol}>{coin.symbol}</p>
+                    <p className={styles.coin_symbol}>{coin.symbol}</p>
 
-            <p className={styles.coin_current}>${coin.market_data.current_price.usd}</p>
+                    <p className={styles.coin_current}>Current: ${coin.market_data.current_price.usd}</p>
+
+                    <p className={styles.coin_homepage}><a href={coin.links.homepage[0]} target="_blank">Home Page</a></p>
+                    <p className={styles.coin_subreddit}><a href={coin.links.subreddit_url} target="_blank">Subreddit</a></p>
                 </div>
             </div>
         </Layout>
@@ -23,8 +26,7 @@ export default Coin;
 export async function getServerSideProps(context) {
     const {id} = context.query
 
-    const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}
-    `)
+    const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
 
     const data = await res.json()
 
